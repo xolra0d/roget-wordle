fe8538612ee83f10a9858f5d46b877a91b6e6539
xolra0d <sn24008@edu.lu.lv>
2025-11-17 16:32:46 +0200

    basic structure in place

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use roget::Wordle;
+
+const GAMES: &str = include_str!("../answers.txt");
+
+fn main() {
+    let w = Wordle::new();
+    for answer in GAMES.split_whitespace() {
+        let guesser = roget::algorithms::Naive::new();
+        w.play(answer, guesser);
+    }
+}
